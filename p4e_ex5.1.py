53a2838e0d23191d413e588e755de9222fb1d1c7
dzejmsjakub <73064954+dzejmsjakub@users.noreply.github.com>
2023-04-16 08:42:54 +0200

    python for everybody course excercise

---
+++ p4e_ex5.1.py
@@ -0,0 +1,29 @@
+num = 0 # number
+tot = 0 # total
+smallest = None
+largest = None
+
+while True:
+    sval = input('Enter a number: ') # stringvalue    
+    
+    if sval == 'done':
+        break 
+    
+    try:
+        fval = float(sval) # float value
+    
+    except:
+        print('Invalid input!')
+        continue  
+    
+    if smallest == None or smallest < fval:
+        smallest = fval
+    
+    if largest == None or largest > fval:
+        largest = fval  
+    
+    num = num + 1 # number counting
+    tot = tot + fval # total counting
+
+print(smallest, largest)
+print(tot, num, tot/num)
