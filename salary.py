71cafde644c007f91fe1c6c16ad7c402e9a88e80
dzejmsjakub <73064954+dzejmsjakub@users.noreply.github.com>
2023-04-16 09:51:30 +0200

    update script - while loop

--- salary.py
+++ salary.py
@@ -1,19 +1,30 @@
-hour = input('Podaj liczbę godzin: ')
+while True:
+    
+    hour = input('Podaj liczbę godzin: ')
+    
+    try:
+        hours = float(hour)
+    
+    except:
+        print('Błąd, podaj wartość numeryczną.')
+        continue
 
-try:
-    hours = float(hour)
+    while True:
+        
+        per_hour = input('Podaj stawkę godzinową: ')
 
-    per_hour = input('Podaj stawkę godzinową: ')
-    per_hours = float(per_hour)
+        try:
+            per_hours = float(per_hour)
+    
+        except:
+            print('Błąd, podaj wartość numeryczną.')
+            continue    
 
-
-    if hours > 40 :
-        salary = 40 * per_hours + (hours - 40) * 1.5 * per_hours
-        print('Wynagrodzenie:',round(salary, 2))
-
-    else :
-        salary = hours * per_hours
-        print('Wynagrodzenie:',round(salary, 2))
-
-except:
-    print('Błąd, podaj wartość numeryczną')
+        if hours > 40 :
+            salary = 40 * per_hours + (hours - 40) * 1.5 * per_hours
+            print('Wynagrodzenie:',round(salary, 2))
+            break
+        else :
+            salary = hours * per_hours
+            print('Wynagrodzenie:',round(salary, 2))
+            break
